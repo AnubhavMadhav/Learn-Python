7dacff089789afa4d146c4b8d8579fb5e7443a82
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-03-16 02:53:03 +0530

    Print and String Formatting Done

--- 4 Print and Input Functions/printfun.py
+++ 4 Print and Input Functions/printfun.py
@@ -16,3 +16,22 @@ print(a,b,sep=',')                           # Output: 10,20
 
 print(a,b,sep='++++')     # used separator   # Output: 10++++20
 
+
+
+
+
+'''Print and String Formatting'''
+
+name = 'Anubhav'
+marks = 90.4678
+
+print("Name is",name, "Marks are",marks)                        # Output: Name is Anubhav Marks are 90.4678
+
+print("Name is %s, Marks are %f"%(name,marks))                  # Output: Name is Anubhav, Marks are 90.467800   
+
+print("Name is %s, Marks are %.2f"%(name,marks))                # Output: Name is Anubhav, Marks are 90.47
+
+print("Name is {}, Marks are {}".format(name,marks))            # Output: Name is Anubhav, Marks are 90.4678
+
+print("Name is {0}, Marks are {1}".format(name,marks))          # Output: Name is Anubhav, Marks are 90.4678
+
