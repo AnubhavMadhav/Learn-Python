1e4c6d0014fcd0f124d55d68f2ef00c1a7d27128
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-03-16 03:24:04 +0530

    Reading Multiple Inputs Done

--- 4 Print and Input Functions/inputfun.py
+++ 4 Print and Input Functions/inputfun.py
@@ -14,3 +14,18 @@ print(type(i))
 i = int(input("Enter an Integer Number:"))
 print(i)
 print(type(i))
+
+
+
+'''Reading Multiple Inputs'''
+lst = [x for x in input("Enter three numbers separated by space:").split()]
+print(lst)
+
+lst = [x for x in input("Enter three numbers separated by comma:").split(',')]
+print(lst)
+
+lst = [int(x) for x in input("Enter three numbers separated by comma:").split(',')]
+print(lst)
+
+lst = [float(x) for x in input("Enter three numbers separated by comma:").split(',')]
+print(lst)
