8b3d8e217f88c173459a5ace348f24a36a910a0f
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-03-15 17:15:47 +0530

    creating a list and it's basics done

---
+++ 2 Sequence Types/listtype.py
@@ -0,0 +1,20 @@
+
+
+# Declaring a List
+l1 = [1,2, 'Anubhav', 10.5, -30]
+
+
+# Print a List
+print(l1)
+
+# Print element of a List at given Index
+print(l1[3])
+
+# Slicing a List
+print(l1[3:5])
+
+# Repitition in a List
+print(l1*4)
+
+# Length of a List
+print(len(l1))
