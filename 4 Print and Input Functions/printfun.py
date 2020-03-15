5a3113592b151ff3827c4a4be773955584b3be79
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-03-16 02:42:53 +0530

    Print Functions Basics

---
+++ 4 Print and Input Functions/printfun.py
@@ -0,0 +1,18 @@
+print()                                      # Output: 
+
+print('Hello')                               # Output: Hello
+
+print('Hello'*3)                             # Output: HelloHelloHello
+
+print('All the Power is with you')           # Output: All the Power is with you
+
+print('All the Power \nis with you')         # Output: All the Power 
+                                                #      is with you
+a,b = 10,20
+print(a,b)                                   # Output: 10 20
+
+
+print(a,b,sep=',')                           # Output: 10,20
+
+print(a,b,sep='++++')     # used separator   # Output: 10++++20
+
