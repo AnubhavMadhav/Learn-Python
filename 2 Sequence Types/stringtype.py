466b4db0f5220994fe8a3ef1400ee0e2788ed481
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-03-15 16:32:29 +0530

    Simple Datatypes and String in Sequence Types Done

---
+++ 2 Sequence Types/stringtype.py
@@ -0,0 +1,25 @@
+# Create a String
+s= "I am Anubhav"
+print(s)
+
+# Multi Line String
+s1 = """I am  Anubhav
+I am a very good boy.
+I am a Computer Science Engineer"""
+print(s1)
+
+# Index of a String
+print(s[2])
+
+# Print a String 2 times
+print(s*2)
+
+# Print a String 3 times
+print(s1*3)
+
+# Print the character at index 3 of a String 2 times
+print(s[3]*2)
+
+# Length of a String
+print(len(s))
+print(len(s1))
