ae4beaa4f7a110a95c73859341eff252077a74c1
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-03-16 03:13:43 +0530

    Input Functions Basics Done

---
+++ 4 Print and Input Functions/inputfun.py
@@ -0,0 +1,16 @@
+s = input()
+print(s)
+
+s = input("Enter your Name:")
+print(s)
+
+
+
+i = input("Enter an Integer Number:")
+print(i)
+print(type(i))
+
+
+i = int(input("Enter an Integer Number:"))
+print(i)
+print(type(i))
