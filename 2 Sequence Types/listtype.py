ca5490e850d0f44f71eb5b41e89e134cf43a6efc
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-03-15 17:35:05 +0530

    Adding or Removing elements in a List Done

--- 2 Sequence Types/listtype.py
+++ 2 Sequence Types/listtype.py
@@ -1,5 +1,5 @@
 
-
+"""Create a List"""
 # Declaring a List
 l1 = [1,2, 'Anubhav', 10.5, -30]
 
@@ -18,3 +18,17 @@ print(l1*4)
 
 # Length of a List
 print(len(l1))
+
+
+
+"""Adding and Removing List Elements"""
+# Append an element
+l1.append(40)           # will add the element at the end of List
+
+# Remove an element 
+l1.remove("Anubhav")    # removes "Anubhav" from l1
+
+# delete an element through it's index by passing the list "del" function
+del(l1[1])              # it is not a function for list, it is an inbuilt function in python in which we can pass a list or anything else
+
+print(l1)
