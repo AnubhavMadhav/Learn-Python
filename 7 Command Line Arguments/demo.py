d57ad0bbd30a4a53ee078e5603132690c2574854
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-03-16 19:50:20 +0530

    Product of 2 numbers using command line is done

--- 7 Command Line Arguments/demo.py
+++ 7 Command Line Arguments/demo.py
@@ -1,8 +1,8 @@
 import sys
 
-lst = sys.argv                      # stores file path
+lst = sys.argv                      # This gives us the List of Command Line Arguments
 
-for i in lst: print(i)
+for i in lst: print(i)              # Prints file path
 
 
  # Click on the "Run configurations" or "Edit Comfigurations" of your IDE and then in the 'Parameters' section pass "123 456 789". Now, you will get a length of 4
