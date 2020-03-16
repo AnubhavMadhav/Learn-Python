7a0791d8404b4b5cb3b5a242a749cad1fcf30b3d
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-03-16 20:22:16 +0530

    Returned the value from a function

--- 8 Functions/average.py
+++ 8 Functions/average.py
@@ -1,4 +1,12 @@
+'''Printed the value from a Function'''
 def average(a,b):
     print("Average pf two numbers is :",(a+b)/2)
 
 average(10,20)
+
+'''Return the value from a Function'''
+def average2(a,b):
+    return (a+b)/2
+
+avg = average2(10,20)
+print(avg)
