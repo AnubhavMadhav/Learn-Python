61737b646def8c2c2324fa559b69437de00eca65
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-03-16 16:35:46 +0530

    Area of Circle Program using Math Module Done

--- 5 Basic Programs/areaofcircle.py
+++ 5 Basic Programs/areaofcircle.py
@@ -1,4 +1,12 @@
+# r = float(input("Enter the radius of the Circle:"))
+# pi = 22/7
+# area = pi * r**2
+# print("Area of the circle with given radius is:",area)
+
+
+
+'''Area of Circle using Math Module'''
+import math
 r = float(input("Enter the radius of the Circle:"))
-pi = 22/7
-area = pi * r**2
+area = math.pi * r**2
 print("Area of the circle with given radius is:",area)
