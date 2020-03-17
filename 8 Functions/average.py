aa5b87dc96b6a795bf75c04367bda58634981d40
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-03-17 21:09:34 +0530

    Done Keyword Arguments

--- 8 Functions/average.py
+++ 8 Functions/average.py
@@ -1,12 +1,21 @@
 '''Printed the value from a Function'''
+# def average(a,b):
+#     print("Average pf two numbers is :",(a+b)/2)
+#
+# average(10,20)
+#
+# '''Return the value from a Function'''
+# def average2(a,b):
+#     return (a+b)/2
+#
+# avg = average2(10,20)
+# print(avg)
+
+'''Using keywords arguments'''
+
 def average(a,b):
-    print("Average pf two numbers is :",(a+b)/2)
-
-average(10,20)
-
-'''Return the value from a Function'''
-def average2(a,b):
+    print(a)
+    print(b)
     return (a+b)/2
 
-avg = average2(10,20)
-print(avg)
+print(average(b=10,a=20))           # this assigns value and the above printed variables are assigned the same values
