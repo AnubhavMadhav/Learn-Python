02f43abc3c7b1e3bd6e407503dac5c6e10339a2a
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-03-17 21:20:49 +0530

    Assigning Default Values to Arguments Done

--- 8 Functions/average.py
+++ 8 Functions/average.py
@@ -13,9 +13,20 @@
 
 '''Using keywords arguments'''
 
-def average(a,b):
+# def average(a,b):
+#     print(a)
+#     print(b)
+#     return (a+b)/2
+#
+# print(average(b=10,a=20))           # this assigns value and the above printed variables are assigned the same values
+
+
+'''Using default arguments'''
+
+def average(a=10,b=40):               # Assigning default values to arguments
     print(a)
     print(b)
-    return (a+b)/2
+    return(a+b)/2
 
-print(average(b=10,a=20))           # this assigns value and the above printed variables are assigned the same values
+print(average())                      # Calling the function without passing arguments, now this works only when default values are assigned to the arguments
+print(average(a=30))                  # Calling the function with only 1 argument passed, now this works only when default values are assigned to the arguments
