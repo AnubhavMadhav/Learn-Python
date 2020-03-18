9d1cadb61afa3252a172879b1c7c3d2f165c088a
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-03-18 22:12:46 +0530

    Created my first class in Python named Product

---
+++ 11 Object Oriented Programming/product.py
@@ -0,0 +1,20 @@
+'''This is my first class in python'''
+
+class Product:
+    def __init__(self):
+        self.name = 'Dell Inspiron'
+        self.description = 'Laptop, i7, 8th Gen, 8 GB RAM, 512 GB SSD'
+        self.price = 91000
+
+p1 = Product()
+
+print(p1.name)
+print(p1.description)
+print(p1.price)
+
+
+p2 = Product()
+
+print(p2.name)
+print(p2.description)
+print(p2.price)
