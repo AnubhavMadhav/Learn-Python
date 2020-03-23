47ee2935e8ec6d70fba3cc2508999230ba698045
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-03-23 17:16:07 +0530

    Duck Typing Demo calling callTalk(obj) function, where 'obj' can be of any class Done

---
+++ 14 Polymorphism/ducktypingdemo.py
@@ -0,0 +1,19 @@
+class Duck:
+    def talk(self):
+        print("Quack Quack")
+
+
+class Human:
+    def talk(self):
+        print("Hello")
+
+
+def callTalk(obj):                      # Calls and Executes the talk() function of the class oh whom object is passed as 'obj'
+    obj.talk()
+
+
+d = Duck()
+callTalk(d)
+
+h = Human()
+callTalk(h)
