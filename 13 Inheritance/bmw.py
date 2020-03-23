ebb0ec10305f6cb95c0f5ce1fe911ba0db5a2629
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-03-23 16:23:57 +0530

    Inheriting Functionality Done

--- 13 Inheritance/bmw.py
+++ 13 Inheritance/bmw.py
@@ -4,11 +4,21 @@ class BMW:
         self.model = model
         self.year = year
 
+    def start(self):                            # Can be called by object of any of it's child class
+        print("Car is Starting")
+
+    def stop(self):                             # Can be called by object of any of it's child class
+        print("Car is stopped")
+
 
 class ThreeSeries(BMW):
     def __init__(self, cruiseControlEnabled, make, model, year):
         BMW.__init__(self, make, model, year)
         self.cruiseControlEnabled = cruiseControlEnabled
+
+    def display(self):
+        print(self.cruiseControlEnabled)
+
 
 class FiveSeries(BMW):
     def __init__(self, parkingAssistEnabled, make, model, year):
@@ -21,7 +31,9 @@ print(t.cruiseControlEnabled)
 print(t.make)
 print(t.model)
 print(t.year)
-
+t.start()                   # Inheriting the function of parent class
+t.stop()                    # Inheriting the function of parent class
+t.display()                 # Called it's own function
 
 f = FiveSeries(False, 'GTX', '19y', 2019)
 print(f.parkingAssistEnabled)
