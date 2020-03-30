649243a117e4ddbaa6073c16f8439da8c4ea2670
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-03-30 20:16:13 +0530

    Regular Expressions Done

--- 18 Regular Expressions/matchingdates.py
+++ 18 Regular Expressions/matchingdates.py
@@ -5,5 +5,5 @@ str = "Take up 1 1-03-2019 one 23 idea.One  567idea at a time 20-1-2021"
 result = re.split(r'\d{1,2}-\d{1,2}-\d{4}', str)            # finds and removes dates
 print(result)
 
-result = re.findall(r'\d{1,2}-\d{1,2}-\d{4}', str)          # finds and prints dates
+result = re.findall(r'\d{1,2}-\d{1,2}-\d{4}', str)          # finds and prints dates okay
 print(result)
