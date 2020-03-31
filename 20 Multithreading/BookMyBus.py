69232ca326bd08a50a4a21f550c2578daf2fd2ef
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-04-01 01:31:36 +0530

    Added more logic

--- 20 Multithreading/BookMyBus.py
+++ 20 Multithreading/BookMyBus.py
@@ -2,17 +2,27 @@ from threading import *
 
 class BookMyBus:
 
-    def buy(self):
-        print("Confirming a Seat")
-        print("Processing the Payment")
-        print("Printing the Ticket")
+    def __init__(self, availableSeats):
+        self.availableSeats = availableSeats
+
+    def buy(self, seatsRequested):
+        print(current_thread().getName())
+        print("Seats Requested by you:", seatsRequested)
+        print("Total Seats Available:", self.availableSeats)
+        if self.availableSeats>=seatsRequested:
+            print("Confirming a Seat")
+            print("Processing the Payment")
+            print("Printing the Ticket")
+            self.availableSeats-=seatsRequested
+        else:
+            print("Sorry. Not enough seats are available")
 
 
-obj = BookMyBus()
+obj = BookMyBus(10)
 
-t1 = Thread(target = obj.buy)
-t2 = Thread(target = obj.buy)
-t3 = Thread(target = obj.buy)
+t1 = Thread(target = obj.buy, args = (3,))          # We need to pass arguments as a list cause thread class expects arguments to be a list
+t2 = Thread(target = obj.buy, args = (4,))
+t3 = Thread(target = obj.buy, args = (4,))
 
 
 t1.start()
