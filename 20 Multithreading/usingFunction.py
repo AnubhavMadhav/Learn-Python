78f9f677113041bb9999c5a69d80a6902f3adb81
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-04-01 00:45:10 +0530

    Printed Thread Names

--- 20 Multithreading/usingFunction.py
+++ 20 Multithreading/usingFunction.py
@@ -1,14 +1,17 @@
-'''Create a Thread using function'''
+'''Create first Thread using function'''
 
-from threading import Thread
+from threading import *
 
 def displayNumbers():
     i = 0
+    print(current_thread().getName())           # This is Thread-1
     while i<=10:
         print(i)
         i+=1
 
-t = Thread(target=displayNumbers)
 
-t.start()
+print(current_thread().getName())               # This is Main() Thread
+t = Thread(target=displayNumbers)               # Syntax: var = Thread(target=function_name,args)
+
+t.start()                                       # This starts the thread
 
